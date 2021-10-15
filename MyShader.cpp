342e5c9f91ce51fa8b38df5bc38b80b2207b3fe3
lpossible <1084440161@qq.com>
2021-10-15 15:22:28 +0800

    modify

--- MyShader.cpp
+++ MyShader.cpp
@@ -10,14 +10,45 @@ MyShader::MyShader(const char* vertexShaderSource, const char* fragmentShaderSou
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
 	glCompileShader(fragmentShader);
-	shader = glCreateProgram();
-	glAttachShader(shader, vertexShader);
-	glAttachShader(shader, fragmentShader);
-	glLinkProgram(shader);
+	shaderID = glCreateProgram();
+	glAttachShader(shaderID, vertexShader);
+	glAttachShader(shaderID, fragmentShader);
+	glLinkProgram(shaderID);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 }
 void MyShader::use()
 {
-	glUseProgram(shader);
+	glUseProgram(shaderID);
+}
+
+void MyShader::setInt(const char* name, int value)
+{
+	glUniform1i(glGetUniformLocation(shaderID, name), value);
+}
+
+void MyShader::loadTexture(const char* filename,int textureId,int format,const char* textureName,int id)
+{
+	//纹理设置
+	stbi_set_flip_vertically_on_load(true);
+	int width, height, nChannel;
+	unsigned char* data = stbi_load(filename, &width, &height, &nChannel, 0);
+	//定义纹理缓冲
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(textureId);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// 为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//将纹理图像复制到纹理对象中
+	glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+	//生成多级渐远纹理
+	glGenerateMipmap(GL_TEXTURE_2D);
+	//
+	stbi_image_free(data);
+	use();
+	setInt(textureName, id);
 }
--- MyShader.h
+++ MyShader.h
@@ -1,12 +1,13 @@
 #pragma once
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
-
+#include"stb_image.h"
 class MyShader
 {
 public:
 	MyShader(const char* vertexShaderSource, const char* fragmentShaderSource);
 	void use();
-private:
-	unsigned int shader;
+	void setInt(const char* name,int value);
+	void loadTexture(const char* filename, int textureId,int format, const char* textureName,int id);
+	unsigned int shaderID;
 };
